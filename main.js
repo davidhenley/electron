4e8a9d1510e42bd655d146bb49832e90eb604d15
David Henley <henleydr@gmail.com>
2018-01-02 17:43:43 -0600

    Added electron-reload

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
+const { app, BrowserWindow } = electron;
+const reload = require('electron-reload')(__dirname);
 const path = require('path');
 const url = require('url');
 
